b8f6f1ae6e895ade8ba125b9de76b9d34dfcd4e3
stef <paulkelele@gmail.com>
2021-07-04 16:17:10 +0200

    change first

--- src/first.c
+++ src/first.c
@@ -4,24 +4,27 @@
 
 void swip(int *a, int *b);
 
-int main(void){
+/* argc: nombres arguments. argv tableau des arguments. argv[0] nom du programme argv[1] 1er argument, etc...*/
+int main(int argc, char ** argv){
 
 FILE * P_FILE; /* pointer sur un fichier*/
-char NOM_FICHIER[50];
+/*char NOM_FICHIER[50];*/
 char NOM_PERSONNE[50];
 char P[50];
 int age;
 int a=10;
 int b=20;
+int count=10;
  
-printf("Entrez le nom du fichier à créer pour enregistrement: ");
-scanf("%s", NOM_FICHIER);
+/*printf("Entrez le nom du fichier à créer pour enregistrement: ");
+scanf("%s", NOM_FICHIER);*/
 
-P_FILE = fopen(NOM_FICHIER, "a");/* on créer le fichier*/ printf("Entrez identty: ");
+P_FILE = fopen(argv[1], "a");/* on créer le fichier*/ printf("Entrez identty: ");
  scanf("%s %s %i", NOM_PERSONNE, P, &age);
+
+for(int i=0;i<count;i++){
  fprintf(P_FILE,"%s,%s,%i\n",NOM_PERSONNE,P,age);
-
-
+}
 
  fclose(P_FILE); /* fermeture du pointer*/
 
